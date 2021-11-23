b72f741bbc36da0189606f6790d289384e207957
Peter Andrews-Briscoe <63236951+PeterAndrewBriscoe@users.noreply.github.com>
2021-11-23 16:14:48 +0000

    small testing advances

--- client/testing/creation.spec.js
+++ client/testing/creation.spec.js
@@ -6,20 +6,51 @@
 const request = require('supertest');
 // import server
 const { expect } = require("@jest/globals");
-const { commentCreation } = require("../js/creation");
+const { commentCreation, reactionCreation } = require("../js/creation");
+const app = require('../../server/server');
 let fullComment = 'Hello there, general kenobi'
 let emptyComment = ' ';
+const cc = commentCreation()
+const rc = reactionCreation()
+let testData = [{
+    title: "What do you call a fish wearing a bowtie?",
+    description: "Sofishticated.",
+    author: "Jakirul",
+    gif: "https://media1.giphy.com/media/3ohs4iUIR6b14Hv9N6/200.gif?cid=474c6cfd2opmtx9o0svu9rngse7qpgdzkpb6sfpgqokwq66q&rid=200.gif&ct=g"
+    
+},
+{
+    title: "Why was the scarecrow promoted?",
+    description: "Because he was outstanding in his field.",
+    author: "Jakirul",
+    gif: "https://media1.giphy.com/media/3ohs4iUIR6b14Hv9N6/200.gif?cid=474c6cfd2opmtx9o0svu9rngse7qpgdzkpb6sfpgqokwq66q&rid=200.gif&ct=g"
+    
+}]
 
 
 describe("creation.js", () => {
     describe('comment creation',()=>
-        it('it rejects an empty comment', ()=>{
-            commentCreation(emptyComment);
-            let body = document.body
-            let form = body.getElementsByClassName('comment-form')
+        it('it lets you make a comment', ()=>{
+            // commentCreation(emptyComment);
+            // let body = document.body
+            // let form = body.querySelector('.content-form').outerHTML
             // let para = form.querySelector('p');
-            expect(commentCreation(emptyComment)).toBeTruthy()
-            expect(body.getElementsByClassName('comment-form').querySelector('p').textContent).toBe("Empty comments are not allowed - please try again!")
+            expect(commentCreation(fullComment)).toBeDefined()
+            // expect((para).textContent.outerHTML).toBe("Empty comments are not allowed - please try again!")
         })
     )
-})
+
+    describe('reaction creation',()=>{
+        beforeAll(() => {
+            reactionCreation(testData)
+        });
+    
+        it('it lets you you add a reaction',()=>{
+            expect(reactionCreation()).toBeDefined()
+        })
+        it('fetches the right reactions',()=>{
+            expect(document.querySelector(".reactions > input").childElementCount).toEqual(3)
+        })
+    })
+}
+)
