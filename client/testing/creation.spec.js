d4235357956dc367182839aa8c1e0a60c2d1f7d8
Peter Andrews-Briscoe <63236951+PeterAndrewBriscoe@users.noreply.github.com>
2021-11-23 14:35:20 +0000

    still working on creation test

--- client/testing/creation.spec.js
+++ client/testing/creation.spec.js
@@ -8,16 +8,18 @@ const request = require('supertest');
 const { expect } = require("@jest/globals");
 const { commentCreation } = require("../js/creation");
 let fullComment = 'Hello there, general kenobi'
-let emptyComment = '';
+let emptyComment = ' ';
 
 
 describe("creation.js", () => {
     describe('comment creation',()=>
         it('it rejects an empty comment', ()=>{
             commentCreation(emptyComment);
-            const para = document.getElementsByClassName("title-warning warning");
+            let body = document.body
+            let form = body.getElementsByClassName('comment-form')
+            // let para = form.querySelector('p');
             expect(commentCreation(emptyComment)).toBeTruthy()
-            expect(para.textContent).toBe("Empty comments are not allowed - please try again!")
+            expect(body.getElementsByClassName('comment-form').querySelector('p').textContent).toBe("Empty comments are not allowed - please try again!")
         })
     )
 })
