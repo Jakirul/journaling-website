b0886e67e75becc3c81e9ae74698aedbbdc6249d
Peter Andrews-Briscoe <63236951+PeterAndrewBriscoe@users.noreply.github.com>
2021-11-25 12:47:51 +0000

    fixed groovy model test with async await function

--- server/test/model.spec.js
+++ server/test/model.spec.js
@@ -1,5 +1,5 @@
 const Post = require('../models/Post.js');
-const { postData, newPost } = require('../data/postData.json')
+const postData = require('../data/postData.json');
 let testData = {
     "title": "What do you call a fish wearing a bowtie?",
     "description": "Sofishticated.",
@@ -7,18 +7,27 @@ let testData = {
     "gif": "https://media1.giphy.com/media/3ohs4iUIR6b14Hv9N6/200.gif?cid=474c6cfd2opmtx9o0svu9rngse7qpgdzkpb6sfpgqokwq66q&rid=200.gif&ct=g"
     
 }
-
-const post = new Post(testData);
+const post = new Post(testData)
+new Post(testData)
 post
+async function postCreation(data){
+    let testPost = new Post(data)
+    postData[0] = testPost
+}
+console.log(post.title)
+
 describe('Post model', () => {
     it('makes an instance of a post which has the right date', () => {
+        
         expect(post.title).toBe('What do you call a fish wearing a bowtie?');
         expect(toString(post.date).substr(0,5)).toEqual(toString(new Date().toUTCString()).substr(0,5));
     });
-    it('puts the new post at the top of the array and has the right id', ()=>{
-        newPost(testData)
+    it('puts the new post at the top of the array', async ()=>{
+        await postCreation(testData)
         expect(postData[0].title).toBe('What do you call a fish wearing a bowtie?');
-        expect(parseInt(postData[0].id)).toBe(postData.length)
+    })
+    it('has the right id', ()=>{
+        expect(postData[0].id).toBe(postData.length) 
     })
 
 });
